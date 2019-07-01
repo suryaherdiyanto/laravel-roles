cbec559a99b2eaa0e83a7780223072c7685acefa
surya herdiyanto <suryaherdiyanto@gmail.com>
2019-07-01 14:21:31 +0800

    init

---
+++ src/RoleServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Surya\Role;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoleServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        
+    }
+
+}
